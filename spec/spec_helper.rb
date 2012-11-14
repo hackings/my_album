85d9c1568c15a038e5026ccfbeb90893d42f4ab2
Vishnu Atrai <me@vishnuatrai.com>
2012-11-15 00:45:34 +0530

    should matchers required

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl'
 require 'rspec/autorun'
+require 'shoulda/matchers/integrations/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
