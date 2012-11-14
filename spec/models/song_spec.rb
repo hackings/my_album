bc671c01cd06bb2fa152bee0755be317a3cd8601
Vishnu Atrai <me@vishnuatrai.com>
2012-11-15 01:08:27 +0530

    association spec for song model

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Song do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it{
+      should belong_to(:album)
+    }
+  end
 end
