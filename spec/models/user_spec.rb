03e8898d3c21bb2a71d6b8b74bb574dc9c1f8b74
Vishnu Atrai <me@vishnuatrai.com>
2012-11-15 00:55:12 +0530

    user model validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,12 @@ describe User do
       should have_many(:albums).dependent(:destroy)
       should have_many(:songs).through(:albums)
     }
-  end  
+  end 
+
+ describe 'Validations' do
+   it {
+     should validate_presence_of(:name)
+     should validate_presence_of(:email)
+   }  
+ end  
 end
