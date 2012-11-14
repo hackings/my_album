36e1d986d2759a12fae0590479cd29246bb84784
Vishnu Atrai <me@vishnuatrai.com>
2012-11-14 21:22:16 +0530

    initial commit - new app created basic strecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAlbum::Application.config.secret_token = '3fef779bc8a766c268baa5f5a35e33f9af2fb7248d2d975c45a19f657072f4ea03ca84d6837930f83012ff9813b0a16b8bfa055b60c9fc363c936e89ea6d33cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyAlbum::Application.config.session_store :cookie_store, key: '_my_album_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyAlbum::Application.config.session_store :active_record_store
